808972318e134b6a6d3e4b028107105c23922d7c
TatheerB <60303339@udst.edu.qa>
2026-02-11 17:40:33 +0300

    Fix lives counter calculation (fixes #6)

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ function updateWrongLetters() {
 
 function updateLives() {
     const livesLeft = gameState.maxWrong - gameState.wrongGuesses;
-    document.getElementById('livesLeft').textContent = `Lives Remaining: ${livesLeft} / ${gameState.maxWrong}`;
+    document.getElementById('livesLeft').textContent = livesLeft;
 }
 
 function updateHangman() {
